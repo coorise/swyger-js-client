a7afc4cdc7d7ff873d4e2ba225d5a5aec746556a
coorise <team.coorise@gmail.com>
2023-06-11 01:32:27 -0700

    Updating Swyger Js Client

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-/*import SwygerAuthClient from '@swyger/client-auth'
+import SwygerAuthClient from '@swyger/client-auth'
 import SwygerDatabaseClient from '@swyger/client-database'
-import SwygerStorageClient from '@swyger/client-storage'*/
+import SwygerStorageClient from '@swyger/client-storage'
 
 
 
 const SwygerClient= {
     init:(ClientConfig)=>{
-        /*const auth = SwygerAuthClient.init(ClientConfig)
+        const auth = SwygerAuthClient.init(ClientConfig)
         const database = SwygerDatabaseClient.init(ClientConfig)
-        const storage = SwygerStorageClient.init(ClientConfig)*/
+        const storage = SwygerStorageClient.init(ClientConfig)
 
         return {
-            /*auth,
+            auth,
             database,
-            storage*/
+            storage
         }
     }
 }
